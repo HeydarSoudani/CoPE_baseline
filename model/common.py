78fc9d3f66c5debd16e283ea714980dcc323e436
Heydar Soudani <heydar.soudani@gmail.com>
2022-02-05 15:06:59 +0330

    c100 added

--- model/common.py
+++ model/common.py
@@ -223,9 +223,9 @@ class Resnet50(nn.Module):
         super(Resnet50, self).__init__()
 
         self.pretrained = models.resnet50(pretrained=True)
-        self.fc1 = nn.Linear(1000, args.n_hiddens)
+        self.fc1 = nn.Linear(1000, 160)
         self.dp1 = nn.Dropout(args.dropout)
-        self.fc2 = nn.Linear(args.n_hiddens, args.n_outputs)
+        self.fc2 = nn.Linear(160, 100)
         self.dp2 = nn.Dropout(args.dropout)
 
         # init the fc layers
